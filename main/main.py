be2d1200b4e01c0e883dc17f838bd744b479a746
Ermes25 <ermes252020@gmail.com>
2024-03-26 09:07:45 -0600

    nuevos cambios en las entidades

--- main/main.py
+++ main/main.py
@@ -1,76 +1,103 @@
 import os
-import dao.daoConnection as dao
-import models.clases as c
+import sys
+sys.path.append(r"C:\Users\ermes\OneDrive\Documentos\LENGUAJES DE PROGRAMACION\SESION_9\ConexionBD\ConexiónBD")
+from dao import daoConnection
+from models import clases as c
+
+
 
 
 os.system('cls')
 
-conex = dao.Connection("localhost", "root", "", "dbregisters")
+conex = daoConnection.Connection("localhost", "root", "", "dbregisters")
 conex.connect()
 
 
-#instanciar modelo
-city1 = c.City("Managua", 1)
-city2 = c.City("León", 1)
-city3 = c.City("Granada", 1)
-city4 = c.City("Masaya", 1)
-city5 = c.City("Estelí", 1)
-city6 = c.City("Jinotepe", 1)
+# def insertarCiudad()
+# nombre = input("INGRESA EL NOMBRE")
+# city = c.City(nombre,1)
 
-#instanciar dao
-daoCity = dao.DaoCity(conex)
-#insertar
-daoCity.insert(city1)
-daoCity.insert(city2)
-daoCity.insert(city3)
-daoCity.insert(city4)
-daoCity.insert(city5)
-daoCity.insert(city6)
+# city1 = c.City(nombre, 1)
 
-cities = daoCity.get_all()
-for city in cities:
-    print(city)
 
-#Instaciar modelo
-Job1 = c.Job("Ingeniero de Software",1)
-Job2 = c.Job("Medico",2)
-Job3 = c.Job("Profesor",3)
-Job4 = c.Job("Abogado",4)
-Job5 = c.Job("Contador",5)
-Job6 = c.Job("Chef",6)
+# daoCity = daoConnection.DaoCity(conex)
 
-#Instaciar dao
-daoJob = dao.DaoJob(conex)
-#Insertar
-daoJob.insert(Job1)
-daoJob.insert(Job2)
-daoJob.insert(Job3)
-daoJob.insert(Job4)
-daoJob.insert(Job5)
-daoJob.insert(Job6)
+# cities = daoCity.get_all()
+# for city in cities:
+#     print(city)
 
-Jobs = daoJob.get_all()
-for jobsj in Jobs:
-    print(jobsj)
+
+# def menu():
+#     print("1. AGREGAR UNA CIUDAD")
+#     print("2. EDITAR CIUDAD")
+#     print("3. MOSTRAR CIUDAD")
+#     print("4. ACTUALIZAR CIUDAD")
+#     print("5. ELIMINAR CIUDAD")
+#     print("6. SALIR")
+
+
+    
 
 #instanciar modelo
-Employee1 = c.Employee("Juan Garcia",21,8,"10000",1)
-Employee2 = c.Employee("Maria Lopez",22,9,"5000",1)
-Employee3 = c.Employee("Pablo Martinez",23,10,"2000",1)
-Employee4 = c.Employee("Carlos Perez",24,11,"6000",1)
-Employee5 = c.Employee("Jose Torres",25,12,"6000",1)
-Employee6 = c.Employee("Patricia Ruiz",26,13,"7000",1)
-#Instanciar dao
-daoEmployee = dao.DaoEmployee(conex)
-#Insertar
-daoEmployee.insert(Employee1)
-daoEmployee.insert(Employee2)
-daoEmployee.insert(Employee3)
-daoEmployee.insert(Employee4)
-daoEmployee.insert(Employee5)
-daoEmployee.insert(Employee6)
-#consultar
 
-Employees = daoEmployee.get_all()
-for employees in Employees:
-    print(employees)
+# city2 = c.City("León", 1)
+# city3 = c.City("Granada", 1)
+# city4 = c.City("Masaya", 1)
+# city5 = c.City("Estelí", 1)
+# city6 = c.City("Jinotepe", 1)
+
+# #instanciar dao
+
+# #insertar
+# daoCity.insert(city1)
+# daoCity.insert(city2)
+# daoCity.insert(city3)
+# daoCity.insert(city4)
+# daoCity.insert(city5)
+# daoCity.insert(city6)
+
+
+
+# #Instaciar modelo
+# Job1 = c.Job("Ingeniero de Software",1)
+# Job2 = c.Job("Medico",2)
+# Job3 = c.Job("Profesor",3)
+# Job4 = c.Job("Abogado",4)
+# Job5 = c.Job("Contador",5)
+# Job6 = c.Job("Chef",6)
+
+# #Instaciar dao
+# daoJob = daoConnection.DaoJob(conex)
+# #Insertar
+# daoJob.insert(Job1)
+# daoJob.insert(Job2)
+# daoJob.insert(Job3)
+# daoJob.insert(Job4)
+# daoJob.insert(Job5)
+# daoJob.insert(Job6)
+
+# Jobs = daoJob.get_all()
+# for jobsj in Jobs:
+#     print(jobsj)
+
+# #instanciar modelo
+# Employee1 = c.Employee("Juan Garcia",21,8,"10000",1)
+# Employee2 = c.Employee("Maria Lopez",22,9,"5000",1)
+# Employee3 = c.Employee("Pablo Martinez",23,10,"2000",1)
+# Employee4 = c.Employee("Carlos Perez",24,11,"6000",1)
+# Employee5 = c.Employee("Jose Torres",25,12,"6000",1)
+# Employee6 = c.Employee("Patricia Ruiz",26,13,"7000",1)
+# #Instanciar dao
+# daoEmployee = daoConnection.DaoEmployee(conex)
+# #Insertar
+# daoEmployee.insert(Employee1)
+# daoEmployee.insert(Employee2)
+# daoEmployee.insert(Employee3)
+# daoEmployee.insert(Employee4)
+# daoEmployee.insert(Employee5)
+# daoEmployee.insert(Employee6)
+# #consultar
+
+# Employees = daoEmployee.get_all()
+# for employees in Employees:
+#     print(employees)
